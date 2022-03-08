3a165e8420b5043d47425ec5f5795741393e9487
joaquinquiroga22 <joaquinquiroga22@gmail>
2022-03-08 13:59:42 -0300

    Cambios2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from './Components/NavBar/Nav';
 function App() {
   return (
-    <div className="App">
-      <h1>Hola</h1>
+    <div >
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Nav/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
